5a48f68c3953b41e0436e2b65812b9508efd009b
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-05 15:47:25 +0300

    task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,12 @@
+const categoriesList = document.querySelector('#categories');
+
+const categoriesItem = categoriesList.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesItem.length}`);
+
+categoriesItem.forEach((item) => {
+    const categoriesTitle = item.querySelector('h2').textContent;
+    console.log(`Category: ${categoriesTitle}`);
+
+    const categoriesElement = item.querySelectorAll('li');
+    console.log(`Elements: ${categoriesElement.length}`);
+})
