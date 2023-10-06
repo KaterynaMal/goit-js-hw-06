09ab0afd81a5e5bdb73de84ce59111c0e6f90399
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-06 17:42:13 +0300

    task 8

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -1,7 +1,7 @@
-const fontSizeControl = document.querySelector('#font-size-control');
-const textSpan = document.querySelector('#text');
+const fontSizeControl = document.querySelector("#font-size-control");
+const textSpan = document.querySelector("#text");
 
-fontSizeControl.addEventListener('input', () => {
-    const fontSize = fontSizeControl.value + 'px';
-    textSpan.style.fontSize = fontSize;
+fontSizeControl.addEventListener("input", () => {
+  const fontSize = fontSizeControl.value + "px";
+  textSpan.style.fontSize = fontSize;
 });
--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,25 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  const formData = new FormData(registerForm);
+  const formDataObject = {};
+
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  if (formDataObject.email === "" || formDataObject.password === "") {
+    return alert("Всі поля мають бути заповненими!");
+  } else {
+    console.log(formDataObject);
+  }
+
+  form.reset();
+}
