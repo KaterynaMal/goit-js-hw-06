89abf1ec6df5d5407c9d9cb7908ef7368db36ac9
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2023-10-06 16:48:44 +0300

    task 6

---
+++ src/js/task-06.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#validation-input');
+const expectedLength = input.getAttribute('data-length');
+
+const updateValidClass = () => {
+    const inputValue = input.value.length;
+
+    if (inputValue == expectedLength) {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+    } else {
+        input.classList.remove('valid');
+        input.classList.add('invalid');
+    }
+};
+
+input.addEventListener('blur', updateValidClass);
